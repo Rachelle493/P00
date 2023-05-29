d58277c4cdcd02f5dd0a1d40f43475f4bd7513ed
22036196 <22036196@myrp.edu.sg>
2023-05-29 14:18:11 +0800

    final-updated

--- P00/src/Calculator.java
+++ P00/src/Calculator.java
@@ -6,4 +6,10 @@ public class Calculator {
 	public int subtract(int a, int b) {
 		return a-b;
 	}
+	public int multiple(int a, int b) {
+		return a*b;
+	}
+	public int divide(int a, int b) {
+		return a/b;
+	}
 }
--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -8,42 +8,51 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
 
 	@Test
-	public void test() {
+	public void testAdd() {
 		//fail("Not yet implemented");
 		int a=1244;
 		int b=8765;
 		
 		Calculator cal=new Calculator();
 		int actual=cal.add(a, b);
+		
 		int expected=9999;
 		assertEquals(expected,actual);
 	}
 	@Test
-	public final void testSubtract() {
+	public void testSubtract() {
 		int a=9876;
 		int b=4321;
 		
 		Calculator cal=new Calculator();
-		int actual= cal.subtract(a, b);
+		int actual=cal.subtract(a, b);
 		
 		int expected=5555;
+		assertEquals(actual,expected);
+	}
+	@Test
+	public void testMultiple() {
+		int a=2;
+		int b=7;
+		
+		Calculator cal=new Calculator();
+		int actual=cal.multiple(a,b);
+		
+		int expected=14;
+		assertEquals(expected,actual);
+		
+	}
+	@Test
+	public final void testDivide() {
+		int a=30;
+		int b=3;
+		
+		Calculator cal=new Calculator();
+		int actual=cal.divide(a, b);
+		
+		int expected=10;
 		assertEquals(actual,expected);
 	}
 
